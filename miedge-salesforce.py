15a2e51168f8d84f6cfedb5efc35eef583c3f9a7
athenacats <chenalonya@gmail.com>
2025-03-08 00:37:29 +0300

    test

--- miedge-salesforce.py
+++ miedge-salesforce.py
@@ -383,6 +383,13 @@ def main():
 
     st.write("Authenticate with **Salesforce** first, then upload your **Excel/CSV** file and push data.")
 
+    from streamlit.components.v1 import html
+    html('''
+       <script>
+        window.top.document.querySelectorAll(`[href*="streamlit.io"]`).forEach(e => e.setAttribute("style", "display: none;"));
+      </script>
+    ''')
+
     st.markdown("""
         <style>
         div.stButton > button:first-child {
