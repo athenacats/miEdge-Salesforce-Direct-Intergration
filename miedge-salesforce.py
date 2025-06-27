c98b63859b0ab909389e2d195794812145412c37
athenacats <chenalonya@gmail.com>
2025-06-27 19:42:49 +0300

    test

--- miedge-salesforce.py
+++ miedge-salesforce.py
@@ -271,7 +271,7 @@ def push_to_salesforce(sf_instance, df, selected_object):
         try:
             st.write(f"➡️ Assigning lead to user: {owner_id}")
             # Push data to Salesforce
-            #sf_instance.__getattr__(selected_object).create(data, headers={"Sforce-Auto-Assign": "FALSE"})
+            sf_instance.__getattr__(selected_object).create(data, headers={"Sforce-Auto-Assign": "FALSE"})
             success_count += 1
 
         except Exception as e:
